d11b050e04134c0a28be019ebbfcc760bb66f249
Private <private@invalid.local>
2026-04-22 22:19:14 +0000

    added error module + new functions for abspath and relpath

---
+++ src/errors.rs
@@ -0,0 +1,14 @@
+use std::path::PathBuf;
+
+pub enum Error {
+    IoError(std::io::Error),
+    NonCanonicalPath(PathBuf),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::IoError(value)
+    }
+}
--- src/fs.rs
+++ src/fs.rs
@@ -1,4 +1,12 @@
+//! This module implements various filesystem wrappers, to garantee safer fs operations.
+//!
+//! ```rust
+//! todo!("add a simple example showcasing this module functionalities!!!")
+//! ```
+
 use std::path::PathBuf;
+
+use crate::errors::{Error, Result};
 
 #[derive(Debug)]
 pub struct AbsPath {
@@ -11,34 +19,28 @@ pub struct RelPath {
 }
 
 impl AbsPath {
-    fn new(path: PathBuf) -> Self {
+    /// Creates new AbsPath from an absolute path.
+    pub fn new(path: PathBuf) -> Result<Self> {
         assert!(path.is_absolute());
-        Self { path }
+        let norm_path = path.canonicalize()?;
+        if norm_path != path {
+            return Err(Error::NonCanonicalPath(path));
+        }
+        Ok(Self { path: norm_path })
     }
 }
 
 impl RelPath {
-    fn new(path: PathBuf) -> Self {
+    /// Creates new RelPath from relative path.
+    pub fn new(path: PathBuf) -> Self {
         assert!(path.is_relative());
         Self { path }
-    }
-}
-
-impl From<PathBuf> for AbsPath {
-    fn from(value: PathBuf) -> Self {
-        Self::new(value)
     }
 }
 
 impl From<AbsPath> for PathBuf {
     fn from(value: AbsPath) -> Self {
         value.path
-    }
-}
-
-impl From<PathBuf> for RelPath {
-    fn from(value: PathBuf) -> Self {
-        Self::new(value)
     }
 }
 
