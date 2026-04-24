2104c92e2965ce42b31baa9168d43e2067c4b4d0
Private <private@invalid.local>
2026-04-24 08:47:43 +0000

    added profile module

--- src/core/profile.rs
+++ src/core/profile.rs
@@ -1,1 +1,44 @@
 //! This module implements structs and methods to handle dotfiles profiles.
+
+/// Represents the profile type.
+#[derive(Debug)]
+pub enum ProfileType {
+    /// Profile storing list of profiles.
+    Composite,
+    /// Special leaf profile with no children.
+    Module,
+}
+
+/// Represents a dotfiles profile.
+#[derive(Debug)]
+pub struct Profile {
+    name: String,
+    entries: Vec<String>,
+    ptype: ProfileType,
+}
+
+impl Profile {
+    /// Create new profile.
+    pub fn new(name: String, entries: Vec<String>, ptype: ProfileType) -> Self {
+        Self {
+            name,
+            entries,
+            ptype,
+        }
+    }
+
+    /// Get profile name.
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// Get profile entries.
+    pub fn entries(&self) -> &[String] {
+        &self.entries
+    }
+
+    /// Get profile type.
+    pub fn ptype(&self) -> &ProfileType {
+        &self.ptype
+    }
+}
