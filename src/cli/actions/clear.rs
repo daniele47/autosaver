400fe7de80f1e84af7b5cd0da2c43a3f5ca3d7ae
Private <private@invalid.local>
2026-05-08 23:22:52 +0000

    added clear command

--- src/cli/actions/clear.rs
+++ src/cli/actions/clear.rs
@@ -12,7 +12,7 @@ impl Runner {
     /// Backup action to list/save/restore files.
     pub fn clear(&mut self) -> Result<()> {
         // check command and flags
-        if self.args.params().len() > 2 {
+        if self.args.params().len() > 1 {
             return self.invalid_cmd_err();
         }
         self.check_flags(
@@ -38,14 +38,14 @@ impl Runner {
         let run_dir = Self::paths("run")?;
 
         // resolve profile into all leafs
-        let profile = self.load_profile(1)?;
+        let profile = String::from(".");
         let mut profile_loader = Self::profile_loader()?;
         let root_profile = profile_loader.load(&profile)?;
         let profiles = root_profile.resolve(&mut profile_loader)?;
 
         // get all tracked paths
         let mut tracked_paths = HashSet::<AbsPath>::new();
-        self.output_main_profile(&profile);
+        self.output_main_profile("clear command");
         for profile in profiles {
             match profile.ptype() {
                 ProfileType::Composite(_) => unreachable!("Composite profile impossible here"),
@@ -68,8 +68,22 @@ impl Runner {
             }
         }
 
-        // TODO: all_files - tracked files
-        println!("{flag_list:#?} {tracked_paths:#?}");
+        // clear all paths in backup and run dir
+        let mut all_paths = backup_dir.all_files(AbsPath::FILTER_FILES)?;
+        all_paths.extend(run_dir.all_files(AbsPath::FILTER_FILES)?);
+        for file in all_paths {
+            let canon_path = file.canonicalize()?;
+            let rel_path = file.to_relative(&Self::paths("root")?)?;
+            let rel_path_str = rel_path.to_str_lossy();
+            if !tracked_paths.contains(&canon_path) {
+                self.inout.writeln(rel_path_str, Self::PATH_UNTRACKED_COL);
+                if !flag_list {
+                    self.prompt("Do you want to delete the untracked file?", |_| {
+                        Ok(file.purge_path(false)?)
+                    })?;
+                }
+            }
+        }
 
         Ok(())
     }
