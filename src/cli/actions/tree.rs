a352eab3e3fbc839515c1d5b3d4f59fde6201d80
Private <private@invalid.local>
2026-05-10 06:27:43 +0000

    tree command already works (still need cleanups and stuff

--- src/cli/actions/tree.rs
+++ src/cli/actions/tree.rs
@@ -6,6 +6,14 @@ use crate::{
 
 const TREE: [&str; 4] = ["│   ", "    ", "├──", "└──"];
 
+fn insert_at(vec: &mut Vec<bool>, index: usize, value: bool) {
+    if index < vec.len() {
+        vec[index] = value;
+    } else {
+        vec.resize(index + 1, value);
+    }
+}
+
 impl Runner {
     /// Help action to render help message.
     pub fn tree(&self) -> Result<()> {
@@ -24,16 +32,19 @@ impl Runner {
         self.output_main_profile(&profile);
 
         // descent into profiles
+        let mut are_last = Vec::<bool>::new();
         root_profile.descend(&mut loader, |ctx| {
             let p = ctx.path;
             let is_last = ctx.stack.last().map(|(p, _)| p) == ctx.path.last();
+            let len = ctx.path.len();
             let line = if is_last { TREE[3] } else { TREE[2] };
-            println!(
-                "{}{}{}",
-                TREE[0].repeat(p.len().saturating_sub(1)),
-                line.repeat(1.min(p.len())),
-                ctx.item.name()
-            );
+            insert_at(&mut are_last, ctx.path.len(), is_last);
+            for i in 1..len {
+                let line = if are_last[i] { TREE[1] } else { TREE[0] };
+                self.inout.write(line, Self::NO_COL);
+            }
+            self.inout.write(line.repeat(1.min(p.len())), Self::NO_COL);
+            self.inout.writeln(ctx.item.name(), Self::NO_COL);
             Ok(())
         })?;
 
