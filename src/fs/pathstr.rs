87b909faf4c34e6b21e5393ec8094df7fafed5e9
Private <private@invalid.local>
2026-05-13 18:28:11 +0000

    implementing methods for PathStr

--- src/fs/pathstr.rs
+++ src/fs/pathstr.rs
@@ -1,6 +1,10 @@
-use std::path::{Component, Path, PathBuf};
+use std::{
+    fmt::Display,
+    path::{Component, Path, PathBuf},
+    str::FromStr,
+};
 
-use anyhow::{Result, bail};
+use anyhow::{Context, Result, bail};
 use derive_getters::Getters;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -16,6 +20,8 @@ pub struct PathStr {
 }
 
 impl PathStr {
+    // CONSTRUCTORS
+
     pub fn new(pathstr: impl AsRef<Path>) -> Result<Self> {
         let pathstr = pathstr.as_ref();
         let kind;
@@ -41,7 +47,7 @@ impl PathStr {
         })
     }
 
-    pub fn new_typed(pathstr: impl AsRef<Path>, kind: PathKind) -> Result<Self> {
+    pub fn new_with_kind(pathstr: impl AsRef<Path>, kind: PathKind) -> Result<Self> {
         let res = Self::new(pathstr)?;
 
         if res.kind != kind {
@@ -50,5 +56,64 @@ impl PathStr {
         }
 
         Ok(res)
+    }
+
+    // IO FUNCTION WRAPPERS
+
+    pub fn canonicalize(&self) -> Result<Self> {
+        match self.kind {
+            PathKind::Absolute => {
+                let canon = self.path.canonicalize().with_context(|| {
+                    format!("Cannot canonicalize path: {}", self.path.display())
+                })?;
+                Self::new_with_kind(canon, PathKind::Absolute)
+            }
+            PathKind::Relative => {
+                let p = self.path.display();
+                bail!("Cannot canonicalize relative path: {p}")
+            }
+        }
+    }
+
+    pub fn file_name(&self) -> Result<Self> {
+        Self::new_with_kind(
+            self.path
+                .file_name()
+                .with_context(|| format!("Could not get basename of: {}", self.path.display()))?,
+            PathKind::Relative,
+        )
+    }
+}
+
+// TRAIT IMPLEMENTATIONS
+
+impl FromStr for PathStr {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
+        Self::new(s)
+    }
+}
+
+impl TryFrom<PathStr> for String {
+    type Error = anyhow::Error;
+
+    fn try_from(value: PathStr) -> std::prelude::v1::Result<Self, Self::Error> {
+        value
+            .path
+            .to_str()
+            .with_context(|| {
+                format!(
+                    "Could not convert to valid utf8 string: {}",
+                    value.path.display()
+                )
+            })
+            .map(str::to_string)
+    }
+}
+
+impl Display for PathStr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.path.display())
     }
 }
