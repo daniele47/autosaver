3ff436b96f40b6bef144c5fbd296bebd7d6452c6
Private <private@invalid.local>
2026-05-13 22:06:12 +0000

    better conversion functions

--- src/fs/rel.rs
+++ src/fs/rel.rs
@@ -1,4 +1,7 @@
-use std::{fmt::Display, path::PathBuf, str::FromStr};
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
 use anyhow::{Result, bail};
 
@@ -12,7 +15,7 @@ pub struct RelPathStr {
 impl RelPathStr {
     pub fn new(path: PathStr) -> Result<Self> {
         // check path is relative
-        if path.as_ref().is_relative() {
+        if !path.as_ref().is_relative() {
             let p = path.as_ref().display();
             bail!("Path is not relative: {p}");
         } else {
@@ -21,18 +24,12 @@ impl RelPathStr {
     }
 }
 
+// CONVERT INTO
 impl TryFrom<PathStr> for RelPathStr {
     type Error = anyhow::Error;
 
     fn try_from(value: PathStr) -> std::prelude::v1::Result<Self, Self::Error> {
         Self::new(value)
-    }
-}
-impl TryFrom<String> for RelPathStr {
-    type Error = anyhow::Error;
-
-    fn try_from(path: String) -> std::prelude::v1::Result<Self, Self::Error> {
-        Self::new(path.try_into()?)
     }
 }
 impl TryFrom<PathBuf> for RelPathStr {
@@ -42,36 +39,41 @@ impl TryFrom<PathBuf> for RelPathStr {
         Self::new(path.try_into()?)
     }
 }
-
-impl TryFrom<RelPathStr> for PathStr {
+impl TryFrom<String> for RelPathStr {
     type Error = anyhow::Error;
 
-    fn try_from(value: RelPathStr) -> std::prelude::v1::Result<Self, Self::Error> {
-        todo!()
+    fn try_from(path: String) -> std::prelude::v1::Result<Self, Self::Error> {
+        Self::new(path.try_into()?)
+    }
+}
+impl FromStr for RelPathStr {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
+        Self::new(PathStr::from_str(s)?)
+    }
+}
+
+// CONVERT FROM
+impl From<RelPathStr> for PathStr {
+    fn from(value: RelPathStr) -> Self {
+        value.path
     }
 }
 impl TryFrom<RelPathStr> for String {
     type Error = anyhow::Error;
 
     fn try_from(value: RelPathStr) -> std::prelude::v1::Result<Self, Self::Error> {
-        todo!()
+        value.path.try_into()
     }
 }
 impl From<RelPathStr> for PathBuf {
-    fn from(value: PathBuf) -> Self {
-        todo!()
+    fn from(value: RelPathStr) -> Self {
+        value.path.into()
     }
 }
-
-// impl FromStr for RelPathStr {
-//     type Err = anyhow::Error;
-//
-//     fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
-//         Self::new(PathStr::from_str(s)?)
-//     }
-// }
-// impl AsRef<Path> for RelPathStr {
-//     fn as_ref(&self) -> &Path {
-//         &self.path
-//     }
-// }
+impl AsRef<Path> for RelPathStr {
+    fn as_ref(&self) -> &Path {
+        self.path.as_ref()
+    }
+}
