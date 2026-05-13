4b025d40c4364837d5930e31ef8c02f5f5e2e849
Private <private@invalid.local>
2026-05-13 16:55:54 +0000

    added anyhow and starting a rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-use std::{env, process::exit};
-
-use autosaver::{
-    cli::{actions::Runner, flags::ParsedArgs},
-    debug,
-};
-
 fn main() {
-    // parse cmdline args
-    let parsed_args = ParsedArgs::parse(env::args().skip(1).collect());
-
-    // get cli runner
-    let runner = Runner::new(parsed_args);
-
-    // run cli
-    if let Err(e) = runner.run() {
-        runner.inout.error(&e);
-        debug!(runner.inout, "\n{}", e.backtrace());
-        exit(1);
-    }
+    println!("test");
 }
