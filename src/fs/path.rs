c19cb01287c1b5e5768b7603fc75e0952e628088
Private <private@invalid.local>
2026-05-13 20:55:10 +0000

    more ergonimic pathstr

--- src/fs/path.rs
+++ src/fs/path.rs
@@ -1,83 +1,43 @@
 use std::{
     fmt::Display,
-    path::{Component, Path, PathBuf},
+    path::{Component, PathBuf},
     str::FromStr,
 };
 
-use anyhow::{Context, Result, bail};
-use derive_getters::{Dissolve, Getters};
+use anyhow::{Ok, Result, bail};
 
-#[derive(Debug, Clone, PartialEq, Eq, Getters, Dissolve)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PathStr {
     path: PathBuf,
 }
 
 impl PathStr {
-    pub fn new(path: PathBuf) -> Result<Self> {
+    fn new_from_pathbuf(path: PathBuf) -> Result<Self> {
         // check path doesn't contain parent directory
         if path.components().any(|c| c == Component::ParentDir) {
-            let p = path.display();
-            bail!("Path string contains parent directory: {p}");
+            bail!("Path string contains parent directory: {}", path.display());
         }
 
         Ok(Self { path })
     }
 
-    pub fn file_name(&self) -> Result<Self> {
-        Self::new(
-            self.path
-                .file_name()
-                .with_context(|| format!("Could not get basename of: {}", self.path.display()))?
-                .into(),
-        )
-    }
-
-    pub fn join(&self, suffix: Self) -> Result<Self> {
-        Self::new(self.path.join(suffix))
+    pub fn new(path: String) -> Result<Self> {
+        Self::new_from_pathbuf(path.into())
     }
 }
 
+// &str ---> PathStr
 impl FromStr for PathStr {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
-        Self::new(s.into())
+        Self::new_from_pathbuf(s.into())
     }
 }
 
-impl TryFrom<String> for PathStr {
-    type Error = anyhow::Error;
-
-    fn try_from(value: String) -> std::prelude::v1::Result<Self, Self::Error> {
-        Self::new(value.into())
-    }
-}
-
-impl TryFrom<PathStr> for String {
-    type Error = anyhow::Error;
-
-    fn try_from(value: PathStr) -> std::prelude::v1::Result<Self, Self::Error> {
-        value
-            .path
-            .to_str()
-            .with_context(|| {
-                format!(
-                    "Could not convert to valid utf8 string: {}",
-                    value.path.display()
-                )
-            })
-            .map(str::to_string)
-    }
-}
-
+// PathStr ---> {}
 impl Display for PathStr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.path.display())
-    }
-}
-
-impl AsRef<Path> for PathStr {
-    fn as_ref(&self) -> &Path {
-        &self.path
     }
 }
