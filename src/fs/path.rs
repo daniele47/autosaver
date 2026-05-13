e2a2534f35a05b7577c224630f775a50a24e8bc9
Private <private@invalid.local>
2026-05-13 21:32:55 +0000

    added conversions for pathstr

--- src/fs/path.rs
+++ src/fs/path.rs
@@ -1,10 +1,10 @@
 use std::{
     fmt::Display,
-    path::{Component, PathBuf},
+    path::{Component, Path, PathBuf},
     str::FromStr,
 };
 
-use anyhow::{Result, bail};
+use anyhow::{Context, Result, bail};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PathStr {
@@ -22,7 +22,20 @@ impl PathStr {
     }
 }
 
-// &str ---> [PathStr]
+impl TryFrom<String> for PathStr {
+    type Error = anyhow::Error;
+
+    fn try_from(path: String) -> std::prelude::v1::Result<Self, Self::Error> {
+        Self::new(path.into())
+    }
+}
+impl TryFrom<PathBuf> for PathStr {
+    type Error = anyhow::Error;
+
+    fn try_from(path: PathBuf) -> std::prelude::v1::Result<Self, Self::Error> {
+        Self::new(path)
+    }
+}
 impl FromStr for PathStr {
     type Err = anyhow::Error;
 
@@ -31,7 +44,30 @@ impl FromStr for PathStr {
     }
 }
 
-// [PathStr] ---> {}
+impl TryFrom<PathStr> for String {
+    type Error = anyhow::Error;
+
+    fn try_from(value: PathStr) -> std::prelude::v1::Result<Self, Self::Error> {
+        value
+            .path
+            .to_str()
+            .with_context(|| {
+                let p = value.path.display();
+                format!("Could not convert to string: {p}")
+            })
+            .map(str::to_string)
+    }
+}
+impl From<PathStr> for PathBuf {
+    fn from(value: PathStr) -> Self {
+        value.path
+    }
+}
+impl AsRef<Path> for PathStr {
+    fn as_ref(&self) -> &Path {
+        &self.path
+    }
+}
 impl Display for PathStr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.path.display())
--- src/fs/rel.rs
+++ src/fs/rel.rs
@@ -1,3 +1,5 @@
+use std::{fmt::Display, str::FromStr};
+
 use derive_getters::{Dissolve, Getters};
 
 use crate::fs::path::PathStr;
@@ -5,4 +7,31 @@ use crate::fs::path::PathStr;
 #[derive(Debug, Clone, PartialEq, Eq, Getters, Dissolve)]
 pub struct RelPathStr {
     path: PathStr,
+}
+
+impl RelPathStr {
+    pub fn new(path: PathStr) -> Result<Self> {
+        // check path doesn't contain parent directory
+        if path.components().any(|c| c == Component::ParentDir) {
+            bail!("Path string contains parent directory: {}", path.display());
+        }
+
+        Ok(Self { path })
+    }
+}
+
+// &str ---> [RelPathStr]
+impl FromStr for RelPathStr {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
+        Self::new(PathStr::from_str(s)?)
+    }
+}
+
+// [RelPathStr] ---> {}
+impl Display for RelPathStr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.path)
+    }
 }
