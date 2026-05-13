fc76c4a8c8d5db47b42bc855b7d0d9baeeb0324c
Private <private@invalid.local>
2026-05-13 20:08:45 +0000

    reorg

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -1,1 +1,5 @@
-pub mod pathstr;
+pub mod abs;
+pub mod path;
+pub mod rel;
+
+// TODO: actual file system operations (create/delete file/dir, copy, diff, ...)
--- src/fs/path.rs
+++ src/fs/path.rs
@@ -7,76 +7,28 @@ use std::{
 use anyhow::{Context, Result, bail};
 use derive_getters::{Dissolve, Getters};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum PathKind {
-    Absolute,
-    Relative,
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, Getters, Dissolve)]
 pub struct PathStr {
     path: PathBuf,
-    kind: PathKind,
 }
 
 impl PathStr {
-    // CONSTRUCTORS
     pub fn new(path: PathBuf) -> Result<Self> {
-        let kind;
-
         // check path doesn't contain parent directory
         if path.components().any(|c| c == Component::ParentDir) {
             let p = path.display();
             bail!("Path string contains parent directory: {p}");
         }
 
-        // get kind
-        if path.is_absolute() {
-            kind = PathKind::Absolute;
-        } else if path.is_relative() {
-            kind = PathKind::Relative;
-        } else {
-            unreachable!("Path string must be absolute or relative!")
-        }
-
-        Ok(Self { path, kind })
-    }
-
-    pub fn new_with_kind(path: PathBuf, kind: PathKind) -> Result<Self> {
-        let res = Self::new(path)?;
-
-        if res.kind != kind {
-            let p = res.path.display();
-            bail!("Path string {p} is not of kind {kind:?}")
-        }
-
-        Ok(res)
-    }
-
-    // IO FUNCTION WRAPPERS
-
-    pub fn canonicalize(&self) -> Result<Self> {
-        match self.kind {
-            PathKind::Absolute => {
-                let canon = self.path.canonicalize().with_context(|| {
-                    format!("Cannot canonicalize path: {}", self.path.display())
-                })?;
-                Self::new_with_kind(canon, PathKind::Absolute)
-            }
-            PathKind::Relative => {
-                let p = self.path.display();
-                bail!("Cannot canonicalize relative path: {p}")
-            }
-        }
+        Ok(Self { path })
     }
 
     pub fn file_name(&self) -> Result<Self> {
-        Self::new_with_kind(
+        Self::new(
             self.path
                 .file_name()
                 .with_context(|| format!("Could not get basename of: {}", self.path.display()))?
                 .into(),
-            PathKind::Relative,
         )
     }
 
@@ -84,8 +36,6 @@ impl PathStr {
         Self::new(self.path.join(suffix))
     }
 }
-
-// TRAIT IMPLEMENTATIONS
 
 impl FromStr for PathStr {
     type Err = anyhow::Error;
