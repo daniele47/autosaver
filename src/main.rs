89f5509137118ea3c2c8e88af472df7c966ebc03
Private <private@invalid.local>
2026-05-14 02:27:31 +0200

    added log example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use anyhow::Result;
+use tracing::{trace, *};
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
 pub mod fs;
 
-fn main() {
-    println!("test");
+fn main() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(fmt::layer().with_timer(fmt::time::ChronoLocal::rfc_3339()))
+        .with(EnvFilter::new("debug"))
+        .init();
+
+    trace!("Test trace");
+    warn!("Wanr");
+    info!("Info");
+    debug!("debug");
+    error!("Err");
+
+    Ok(())
 }
