ca39cc5e23855631bd729d4519eceba49eb28789
Private <private@invalid.local>
2026-05-14 20:48:56 +0200

    wip parser

--- src/prof/parser.rs
+++ src/prof/parser.rs
@@ -13,24 +13,36 @@ enum RawProfileLine<'a> {
 struct RawProfile<'a> {
     lines: Vec<RawProfileLine<'a>>,
     kind: &'a str,
+    name: &'a str,
+    id: &'a str,
 }
 
 impl<'a> RawProfile<'a> {
     #[instrument(ret, level = "trace")]
-    pub fn parse_config(config: &'a str) -> Self {
+    pub fn parse_config(config: &'a str, name: &'a str) -> Self {
         let mut lines = Vec::new();
         let mut kind = "";
+        let mut id = name;
 
         for line in config.lines() {
+            // option lines
             if let Some(option) = line.strip_prefix("/!") {
                 let option = option.trim();
+                // specific shared options
                 if let Some(kind_str) = option.strip_prefix("kind") {
                     let kind_str = kind_str.trim();
                     kind = kind_str;
-                } else {
+                } else if let Some(id_str) = option.strip_prefix("id") {
+                    let id_str = id_str.trim();
+                    id = id_str;
+                }
+                // fallback to storing not shared options
+                else {
                     lines.push(RawProfileLine::Option(option));
                 }
-            } else if !line.starts_with("/") {
+            }
+            // data lines
+            else if !line.starts_with("/") {
                 if !line.starts_with("//") {
                     warn!("Comments are meant to start with // ({line})");
                 }
@@ -38,13 +50,18 @@ impl<'a> RawProfile<'a> {
             }
         }
 
-        Self { lines, kind }
+        Self {
+            lines,
+            kind,
+            name,
+            id,
+        }
     }
 }
 
 impl Profile {
-    pub fn parse_profile(config: &str) -> Result<Profile> {
-        let raw = RawProfile::parse_config(config);
+    pub fn parse_profile(config: String, name: String) -> Result<Profile> {
+        let raw = RawProfile::parse_config(&config, &name);
         match raw.kind {
             "" | "composite" => {}
             "module" => {}
