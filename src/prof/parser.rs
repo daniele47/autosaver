3030bdaececbca3283b1bc7a862a211d731d9e8a
Private <private@invalid.local>
2026-05-14 20:22:44 +0200

    implemented raw parser

--- src/prof/parser.rs
+++ src/prof/parser.rs
@@ -1,1 +1,30 @@
+use tracing::instrument;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum RawProfileLine<'a> {
+    Option(&'a str),
+    Data(&'a str),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct RawProfile<'a> {
+    lines: Vec<RawProfileLine<'a>>,
+}
+
+impl<'a> RawProfile<'a> {
+    #[instrument(ret, level = "trace")]
+    pub fn parse_config(config: &'a str) -> Self {
+        let mut lines = Vec::new();
+
+        for line in config.lines() {
+            if let Some(option) = line.strip_prefix("/!") {
+
+                lines.push(RawProfileLine::Option(option.trim()));
+            } else if !line.starts_with("/") {
+                lines.push(RawProfileLine::Data(line.trim()));
+            }
+        }
+
+        Self { lines }
+    }
+}
