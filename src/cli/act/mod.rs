a6ee0a9fc4d87369fe004474ad67de25c4602511
Private <private@invalid.local>
2026-05-21 20:50:05 +0200

    added perf times to verbose output

--- src/cli/act/mod.rs
+++ src/cli/act/mod.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use crate::{
     cli::{
         Cli,
@@ -10,10 +12,12 @@ pub mod tree;
 
 impl Cli {
     pub fn run_cmd(&self) -> anyhow::Result<()> {
+        let start = Instant::now();
         let ctx = CliContext::new(&self.home, &self.root, &self.profile)?;
 
         // verbose output
         if self.verbose {
+            verbose!("Configs parsed in {}s", start.elapsed().as_secs_f64());
             verbose!("Current profile: {}", ctx.curr_prof().display());
             verbose!("Home directory: {}", ctx.path(&Paths::Home).display());
             verbose!("Root directory: {}", ctx.path(&Paths::Root).display());
@@ -21,6 +25,7 @@ impl Cli {
             verbose!("Config directory: {}", ctx.path(&Paths::Config).display());
             verbose!("Run directory: {}", ctx.path(&Paths::Run).display());
         }
+        let start = Instant::now();
 
         match self.cmd {
             super::CliCmd::List => todo!(),
@@ -30,6 +35,13 @@ impl Cli {
             super::CliCmd::Run => todo!(),
             super::CliCmd::Tree { .. } => self.action_tree(&ctx),
             super::CliCmd::Clear => todo!(),
+        }?;
+
+        // show run time on verbose output
+        if self.verbose {
+            verbose!("Command run in {}s", start.elapsed().as_secs_f64());
         }
+
+        Ok(())
     }
 }
